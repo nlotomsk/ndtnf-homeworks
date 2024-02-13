f7f8ff2c6c927aefd42b0bcdc721731c47934e08
Slava <nlo_tomsk@mail.ru>
2024-02-13 22:42:06 +0700

    work_7_2

--- 008-nestjs-db/008-01/dist/interfaces/book.create.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateBook = void 0;
-class CreateBook {
-}
-exports.CreateBook = CreateBook;
-//# sourceMappingURL=book.create.js.map
